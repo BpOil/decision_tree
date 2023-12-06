8c120aa4034cb8622f18e2511c014b892074ffb1
Buddy Martin <bpmartin97@gmail.com>
2023-12-06 01:59:58 -0600

    added python script to perform fft on http traffic

---
+++ fft/fft.py
@@ -0,0 +1,29 @@
+import numpy as np 
+import plotly.express as px
+import pandas as pd 
+
+# tshark -r ./2015-03-17/snort.log.1426550408 -Y "http.request.method == GET && ip.src == 192.168.0.0/24" -T fields -e frame.time -e http.host -e ip.src -E header=y -E separator=, -E quote=d > fft/http_internal.csv
+
+# data came from netresec.com/?page=PcapFiles "Network Forensics"
+
+file_name = "./http_internal.csv"
+period = '1min' #1min or 30S
+sample_rate = 30
+
+orgdata = pd.read_csv(f"{file_name}",index_col='frame.time', parse_dates=True)
+data = orgdata['http.host']
+countsperperiod = data.resample(period).count()
+print(f"length of countsperperiod: {len(countsperperiod)}")
+print(f"countsperperiod: {countsperperiod}")
+
+rfft = np.fft.rfft(countsperperiod)
+frequencies = np.fft.rfftfreq(len(countsperperiod),d=(1/sample_rate))
+#print(f"length of rfft: {len(rfft)}")
+#print(f"length of frequencies: {len(frequencies)}")
+#print(f"rfft: {rfft}")
+#print(f"frequencies: {frequencies}")
+
+fig = px.line(x=frequencies, y=(abs(rfft)))
+fig.show()
+
+
